a6083acf431b707237d176181c2abe5e9e397c32
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2022-09-02 16:34:43 -0500

    Scripts added. Imports and service object for chromedriver

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from selenium import webdriver
 import page
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service('usr/bin/chromedriver')
+driver = webdriver.Chrome(service=service)
 
 if __name__ == '__main__':
     pass
