1c4c399e541d7d35c53a1e71ad057c794637adb1
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2022-09-09 17:59:39 -0500

    init method added for driver

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import page
 from selenium import webdriver
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("no-sandbox")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=800,600")
-options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        options=options
-        )
+class setUp:
 
+    def __init__(self, website):
+        options = webdriver.ChromeOptions()
+        options.add_argument("no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=800,600")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Remote(
+                command_executor='http://localhost:4444/wd/hub',
+                options=options
+                )
+        self.website = website
+        self.driver.get(website)
 
-def epochtimes():
-    driver.get("https://www.theepochtimes.com/us-news")
-    print(driver.title)
-    page.MainPage.print_us_top()
+    def get_title(self):
+        print(self.driver.title)
+    # page.MainPage.print_us_top()
 
 
 if __name__ == '__main__':
-    epochtimes()
+    epoch_times = setUp("https://www.theepochtimes.com/us-news")
+    print(epoch_times.get_title())
